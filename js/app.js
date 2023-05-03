f102764fc4915c598d755c87d3cf40dcfb76c838
Joshua Nobleza <joshnobleza@bell.net>
2023-05-03 15:05:44 -0400

    feat: Add functionality to distinguish the section in view

--- js/app.js
+++ js/app.js
@@ -1,17 +1,48 @@
 const navBar = document.querySelector('.navbar');
 const sections = document.querySelectorAll('section');
 
+function setActiveSection(sec) {
+    const notActiveLink = document.querySelector('.active');
+    notActiveLink.classList.remove('active');
+
+    for (const link of navBar.childNodes) {
+        if (link.innerHTML === sec.dataset.nav) {
+            link.classList.add('active');
+            break;
+        }
+    }
+}
+
 function navMenu() {
     const fragment = document.createDocumentFragment();
 
     for (const section of sections) {
         const navItem = document.createElement('li');
-        navItem.textContent = section.dataset.nav;
+        navItem.innerHTML = section.dataset.nav;
 
         fragment.appendChild(navItem);
     }
 
+    // highlight the first section as active in the navbar
+    fragment.childNodes[0].classList.add('active');
+
     navBar.appendChild(fragment);
 }
+
+document.addEventListener('scroll', () => {
+    
+    for (const section of sections) {
+        const rect = section.getBoundingClientRect();
+
+        if (rect.top >= -700) {
+            const notActive = document.querySelector('.active-sec');
+            notActive.classList.remove('active-sec');
+
+            section.classList.add('active-sec');
+            setActiveSection(section);
+            break;
+        }
+    }
+});
 
 navMenu();
